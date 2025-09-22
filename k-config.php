4d27d0f25d9bab3c49a9680d137f1003f4a9814c
codermasterdev <codermaster.dev@gmaik.com>
2025-09-22 14:21:23 -0300

    Primeiro commit do projeto Karenderia

---
+++ k-config.php
@@ -0,0 +1,13 @@
+<?php
+define( 'DB_NAME', 'karenderiabd' );
+define( 'DB_USER', 'karenderiasus' );
+define( 'DB_PASSWORD', 'Gui0307' );
+define( 'DB_HOST', 'ilicnea_karenderia' );
+define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_PREFIX', 'st_' );
+
+define( 'KMRS_DEFAULT_LANGUAGE', 'en' );
+define( 'BACKOFFICE_FOLDER', 'backoffice' );
+define( 'CRON_KEY', '6EE1E6391DC4EF7E8887444E889418B4' );
+define( 'DEMO_MODE', false );
+define( 'DEMO_MERCHANT', array() );
